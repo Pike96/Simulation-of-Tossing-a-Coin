c257a62379860c8db9f43d9105a87bda3d454bc5
Yucheng Lin <Pike96@users.noreply.github.com>
2017-01-14 15:10:28 -0800

    Update Q1.m
    Realised. Show all results in matrix

--- Q1.m
+++ Q1.m
@@ -1,13 +1,26 @@
 clear;
 N=input('Please enter the times of tossing the coin N=');
-L=0;        % L is the longest run of heads
-H=0;        % H is the times of heads
-T=0;        % T is the times of tails
+R=1;        % L is the longest run of heads
+r=1;        % r is the temporary run of heads
+% Set to 1 because R and r should count the previous toss
+
+t=0;        % t records the results of last toss, 1 for head, -1 for tail
+x=[];       % x show all the tossing results
 for i=1:N
-    if rand>0.5
-        H=H+1;
-    else
-        T=T+1;
+    if rand>0.5     % head
+        if t==1     % if previous toss is head
+            r=r+1;
+            if r>=R     % if the current run is the longest
+                R=R+1;
+            end
+        end
+        t=1;
+        x=[x,1];
+    else            % tail
+        r=0;        % clear the temporary run of heads
+        t=-1;
+        x=[x,0];
     end
 end
-disp(['The longest run of heads is: ',num2str(H),num2str(T)]);
+x        % show all results
+disp(['The longest run of heads is: ',num2str(R)]);
