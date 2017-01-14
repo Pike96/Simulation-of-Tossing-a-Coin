2df11c4459321f60a76700c74678d88c2264f54c
Yucheng Lin <Pike96@users.noreply.github.com>
2017-01-14 15:45:22 -0800

    Update Q1.m
    Finished

--- Q1.m
+++ Q1.m
@@ -1,11 +1,14 @@
 clear;
-N=input('Please enter the times of tossing the coin N=');
-R=1;        % L is the longest run of heads
-r=1;        % r is the temporary run of heads
+N=50;       % Times of the events
+t=0;        % t records the results of last toss, 1 for head, -1 for tail
+
+R=1;        % L is the longest run of head
+r=1;        % r is the temporary run of head
 % Set to 1 because R and r should count the previous toss
 
-t=0;        % t records the results of last toss, 1 for head, -1 for tail
-x=[];       % x show all the tossing results
+H=0;        % Times of head
+T=0;        % Times of tail
+
 for i=1:N
     if rand>0.5     % head
         if t==1     % if previous toss is head
@@ -15,12 +18,15 @@ for i=1:N
             end
         end
         t=1;
-        x=[x,1];
+        H=H+1;
     else            % tail
-        r=0;        % clear the temporary run of heads
+        r=0;        % clear the temporary run of head
         t=-1;
-        x=[x,0];
+        T=T+1;
     end
 end
-x        % show all results
-disp(['The longest run of heads is: ',num2str(R)]);
+disp(['The longest run of head is: ',num2str(R)]);
+
+x=[H;T];
+bar(x)
+set(gca,'XTicklabel',['Heads'; 'Tails'])
