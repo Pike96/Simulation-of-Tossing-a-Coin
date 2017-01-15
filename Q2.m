32e4cd486d499cbe373813967c42bd4a981aedb1
Yucheng Lin <Pike96@users.noreply.github.com>
2017-01-14 22:54:26 -0800

    Change the way of X

--- Q2.m
+++ Q2.m
@@ -1,26 +1,18 @@
 clear;
-N=200;       % N is the times of the events
-t=1;        % t records the results of last toss, 1 for head, -1 for tail
-% Set to 1 because the first one should be count
-
-R=0;        % R is the longest run of heads
-r=0;        % r is the temporary run of heads
-
-X=[];       % X show all the tossing results
+N=200;               % N is the times of the events
+R=0;                % R is the longest run of heads
+r=0;                % r is the temporary run of heads
+X=rand(1,N);        % X show all the tossing results
 for i=1:N
-    if rand<0.8     % 80% probablity of heads
-        if t==1     % if previous toss is heads
-            r=r+1;
-            if r>=R     % if the current run is the longest
-                R=R+1;
-            end
+    if X(i)<0.8     % head
+        r=r+1;
+        if r>R     % if the current run is the longest
+            R=R+1;
         end
-        t=1;
-        X=[X 1];
+        X(i)=1;
     else            % tail
         r=0;        % clear the temporary run of heads
-        t=0;
-        X=[X 0];
+        X(i)=0;
     end
 end
 disp(['The number of head is: ',num2str(sum(X))]);
@@ -29,5 +21,5 @@ disp(['The longest run of head is: ',num2str(R)]);
 hist(X)
 set(gca,'XTick',[0; 1])
 set(gca,'XTicklabel',['Tails'; 'Heads'])    % Show names instead of numbers
-text(0.03,N-sum(X)+4,num2str(N-sum(X)))
-text(0.92,sum(X)+4,num2str(sum(X)))         % Show the numbers of H&Ts
+text(0.03,N-sum(X)+1,num2str(N-sum(X)))
+text(0.93,sum(X)+1,num2str(sum(X)))         % Show the numbers of H&Ts
