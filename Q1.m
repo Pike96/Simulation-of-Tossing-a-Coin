e0e6571192f86f2cbb6334b3e476210e1ad81f5d
Yucheng Lin <Pike96@users.noreply.github.com>
2017-01-14 21:50:36 -0800

    Show the number of heads

--- Q1.m
+++ Q1.m
@@ -2,14 +2,14 @@ clear;
 N=50;       % N is the times of the events
 t=0;        % t records the results of last toss, 1 for head, -1 for tail
 
-R=1;        % R is the longest run of head
-r=1;        % r is the temporary run of head
+R=1;        % R is the longest run of heads
+r=1;        % r is the temporary run of heads
 % Set to 1 because R and r should count the previous toss
 
 X=[];       % X show all the tossing results
 for i=1:N
     if rand>0.5     % head
-        if t==1     % if previous toss is head
+        if t==1     % if previous toss is heads
             r=r+1;
             if r>=R     % if the current run is the longest
                 R=R+1;
@@ -18,11 +18,12 @@ for i=1:N
         t=1;
         X=[X 1];
     else            % tail
-        r=0;        % clear the temporary run of head
+        r=0;        % clear the temporary run of heads
         t=0;
         X=[X 0];
     end
 end
+disp(['The number of head is: ',num2str(sum(X))]);
 disp(['The longest run of head is: ',num2str(R)]);
 
 hist(X)
