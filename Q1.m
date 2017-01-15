4c821f7248c1038834bfea145bb2d0653b0faae1
Yucheng Lin <Pike96@users.noreply.github.com>
2017-01-14 16:36:32 -0800

    After referencing piazza

--- Q1.m
+++ Q1.m
@@ -6,9 +6,7 @@ R=1;        % L is the longest run of head
 r=1;        % r is the temporary run of head
 % Set to 1 because R and r should count the previous toss
 
-H=0;        % Times of head
-T=0;        % Times of tail
-
+X=[];       % x show all the tossing results
 for i=1:N
     if rand>0.5     % head
         if t==1     % if previous toss is head
@@ -18,15 +16,17 @@ for i=1:N
             end
         end
         t=1;
-        H=H+1;
+        X=[X 1];
     else            % tail
         r=0;        % clear the temporary run of head
-        t=-1;
-        T=T+1;
+        t=0;
+        X=[X 0];
     end
 end
 disp(['The longest run of head is: ',num2str(R)]);
 
-x=[H;T];
-bar(x)
-set(gca,'XTicklabel',['Heads'; 'Tails'])
+hist(X)
+set(gca,'XTick',[0; 1])
+set(gca,'XTicklabel',['Tails'; 'Heads'])    % Show names instead of numbers
+text(0.03,N-sum(X)+1,num2str(N-sum(X)))
+text(0.93,sum(X)+1,num2str(sum(X)))         % Show the numbers of H&Ts
