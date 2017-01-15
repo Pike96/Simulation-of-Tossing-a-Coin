6139e4c87005f684d67386a3454f46eedf768c03
Yucheng Lin <Pike96@users.noreply.github.com>
2017-01-14 16:37:32 -0800

    Small edit

--- Q1.m
+++ Q1.m
@@ -1,12 +1,12 @@
 clear;
-N=50;       % Times of the events
+N=50;       % N is the times of the events
 t=0;        % t records the results of last toss, 1 for head, -1 for tail
 
-R=1;        % L is the longest run of head
+R=1;        % R is the longest run of head
 r=1;        % r is the temporary run of head
 % Set to 1 because R and r should count the previous toss
 
-X=[];       % x show all the tossing results
+X=[];       % X show all the tossing results
 for i=1:N
     if rand>0.5     % head
         if t==1     % if previous toss is head
