6c67e2dcb8e1c2bed9833a4ae164e76370e55821
Yucheng Lin <Pike96@users.noreply.github.com>
2017-01-14 22:42:17 -0800

    Correct longest run judgement

--- Q1.m
+++ Q1.m
@@ -1,29 +1,20 @@
 clear;
-N=50;       % N is the times of the events
-t=1;        % t records the results of last toss, 1 for head, -1 for tail
-% Set to 1 because the first one should be count
-
+N=10;       % N is the times of the events
 R=0;        % R is the longest run of heads
 r=0;        % r is the temporary run of heads
-
 X=[];       % X show all the tossing results
 for i=1:N
     if rand>0.5     % head
-        if t==1     % if previous toss is heads
-            r=r+1;
-            if r>=R     % if the current run is the longest
-                R=R+1;
-            end
+        r=r+1;
+        if r>R     % if the current run is the longest
+            R=R+1;
         end
-        t=1;
         X=[X 1];
     else            % tail
         r=0;        % clear the temporary run of heads
-        t=0;
         X=[X 0];
     end
 end
-X
 disp(['The number of head is: ',num2str(sum(X))]);
 disp(['The longest run of head is: ',num2str(R)]);
 
